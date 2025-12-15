5770dac13c0c70733651d020c6ffa06134d0eba6
29cmb <devcmb@outlook.com>
2025-12-15 18:58:19 -0500

    feat: :tada: And so it begins.

---
+++ src/main/kotlin/xyz/devcmb/achievementsMC/AchievementsMC.kt
@@ -0,0 +1,14 @@
+package xyz.devcmb.achievementsMC
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AchievementsMC : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
