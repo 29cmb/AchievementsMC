4018cc04eac07f3f4e69e3d107f945c7442abce4
29cmb <devcmb@outlook.com>
2026-01-26 20:33:20 -0500

    docs: Basic readme and disable dev mode

---
+++ README.md
@@ -0,0 +1,5 @@
+![AchievementsMC](./docs/thumbnail.png)
+# Achievements MC
+A simple minecraft plugin for server owners to create achievements for players to work towards
+
+[Video demo](https://youtu.be/vzvP3moF748)
--- src/main/kotlin/xyz/devcmb/achievementsMC/Constants.kt
+++ src/main/kotlin/xyz/devcmb/achievementsMC/Constants.kt
@@ -1,6 +1,6 @@
 package xyz.devcmb.achievementsMC
 
 object Constants {
-    const val VERSION: String = "1.0-0000ad"
-    val IS_DEVELOPMENT: Boolean = true
+    const val VERSION: String = "1.0-0000b0"
+    val IS_DEVELOPMENT: Boolean = false
 }
